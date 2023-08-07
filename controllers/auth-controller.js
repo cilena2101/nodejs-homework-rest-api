996bcd2bf6f33590e0b813a52e8c2d8f5d53b9c4
Elena Potoskaya <115727150+cilena2101@users.noreply.github.com>
2023-08-07 23:43:18 +0300

    work done

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,14 +1,16 @@
 import bcrypt from "bcryptjs";
-
 import jwt from "jsonwebtoken";
-
+import path from "path";
+import fs from "fs/promises";
 import { User } from "../models/index.js";
-
 import { HttpError } from "../helpers/index.js";
-
 import { ctrlWrapper } from "../decorators/index.js";
+import gravatar from "gravatar";
+import Jimp from "jimp";
 
 const { JWT_SECRET } = process.env;
+
+const avatarPath = path.resolve("public", "avatars");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -18,7 +20,12 @@ const signup = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     name: newUser.name,
@@ -31,7 +38,7 @@ const signin = async (req, res) => {
   const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
-  };
+  }
 
   const passwordCompare = bcrypt.compare(password, user.password);
   if (!passwordCompare) {
@@ -44,7 +51,7 @@ const signin = async (req, res) => {
 
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
 
-  await User.findByIdAndUpdate(user._id, {token});
+  await User.findByIdAndUpdate(user._id, { token });
   res.json({
     token,
   });
@@ -56,21 +63,38 @@ const getCurrent = (req, res) => {
   res.json({
     name,
     email,
-  })
+  });
 };
 
-const signout = async(req, res) =>{
-    const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token:""});
+const signout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-    res.json({
-        message: "Signout suсcess"
-    })
-}
+  res.json({
+    message: "Signout suсcess",
+  });
+};
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: oldPath, filename } = req.file;
+  const newPath = path.join(avatarPath, filename);
+
+  const resizeAvatar = await Jimp.read(oldPath);
+  resizeAvatar.resize(250, 250);
+  await fs.rename(oldPath, newPath);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
 
 export default {
   signup: ctrlWrapper(signup),
   signin: ctrlWrapper(signin),
   getCurrent: ctrlWrapper(getCurrent),
-  signout: ctrlWrapper(signout)
+  signout: ctrlWrapper(signout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
